ffb2d26958ad13f0f7d115e706accaee0676b977
dennyswamb <dennyswamb@localhost.localdomain>
2025-05-29 14:13:50 +0300

    feat:- Update database connection configuration4

--- backend/config/db_config.php
+++ backend/config/db_config.php
@@ -6,11 +6,4 @@ return [
     'password' => getenv('DB_PASSWORD') ?: '^*=k.2CK3w!2s2KDUDKD>',
     'port' => (int)(getenv('DB_PORT') ?: 3306),
     'ssl_ca' => getenv('SSL_CA_PATH') ?: __DIR__ . '/DigiCertGlobalRootCA.crt.pem',
-    'options' => [
-        PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/DigiCertGlobalRootCA.crt.pem',
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 15,
-        PDO::ATTR_PERSISTENT => false
-    ]
-];
+   ];
