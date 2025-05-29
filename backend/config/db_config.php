7d82aa430e45e9b554034ad962ee4e3d3d3fced5
dennyswamb <dennyswamb@localhost.localdomain>
2025-05-29 19:48:58 +0300

    feat: config4

--- backend/config/db_config.php
+++ backend/config/db_config.php
@@ -3,7 +3,7 @@ return [
     'host' => getenv('DB_HOST') ?: 'scoringsystem.mysql.database.azure.com',
     'dbname' => getenv('DB_NAME') ?: 'scoring_system',
     'username' => getenv('DB_USERNAME') ?: 'dennys',
-    'password' => getenv('DB_PASSWORD') ?: '^*=k.2CK3w!2s2KDUDKD>',
+    'password' => getenv('DB_PASSWORD') ?: '',
     'port' => (int)(getenv('DB_PORT') ?: 3306),
     'ssl_ca' => getenv('SSL_CA_PATH') ?: __DIR__ . '/DigiCertGlobalRootCA.crt.pem',
     'options' => [
